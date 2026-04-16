13414b67b9453d761c11b41f3882c6544beb76fe
Lumen-Park <gaiuslumen@gmail.com>
2026-04-16 12:16:24 +0530

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,33 @@
+"""
+Aetherion Logger – JSON structured logs for observability.
+"""
+
+import json
+import os
+import time
+from typing import Any, Dict
+
+class AetherionLogger:
+    def __init__(self, log_dir: str = "./logs"):
+        self.log_dir = log_dir
+        os.makedirs(log_dir, exist_ok=True)
+        self.log_file = os.path.join(log_dir, f"aetherion_{int(time.time())}.jsonl")
+    
+    def log(self, level: str, message: str, **kwargs):
+        entry = {
+            "timestamp": time.time(),
+            "level": level,
+            "message": message,
+            **kwargs
+        }
+        with open(self.log_file, 'a') as f:
+            f.write(json.dumps(entry) + "\n")
+    
+    def info(self, msg: str, **kwargs):
+        self.log("INFO", msg, **kwargs)
+    
+    def error(self, msg: str, **kwargs):
+        self.log("ERROR", msg, **kwargs)
+    
+    def debug(self, msg: str, **kwargs):
+        self.log("DEBUG", msg, **kwargs)
