cabe8e39091bcf3f2884c2a11fd0242d1178da23
Lumen-Park <gaiuslumen@gmail.com>
2026-05-01 12:38:56 +0530

    Create main.js

---
+++ aetherion-desktop/main.js
@@ -0,0 +1,42 @@
+```javascript
+const { app, BrowserWindow, Menu, Tray, nativeImage, shell } = require('electron');
+const path = require('path');
+
+let mainWindow;
+let tray;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 900,
+    minHeight: 600,
+    title: 'Aetherion',
+    icon: path.join(__dirname, 'assets', 'icon.png'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  // Connect to local or remote Aetherion server
+  const serverUrl = process.env.AETHERION_SERVER || 'http://localhost:8000';
+  mainWindow.loadURL(serverUrl);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
